ba354b4c06bcd607da9f988251de799030e2a308
Alexei <alexeikh14@gmail.com>
2025-08-04 18:23:17 +0300

    buttons 1

--- my-to-do-app/src/components/Modal.jsx
+++ my-to-do-app/src/components/Modal.jsx
@@ -1,18 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Modal as BootstrapModal, Button } from "react-bootstrap";
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
 
-export default function Modal({show, onClose}) {
+export default function Modal({show, onClose, onSave}) {
+    const [taskName, setTaskName] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+    const [taskPriority, setTaskPriority] = useState("High");
+
+    const handleSave = (() => {
+        const newTask = {
+            name: taskName,
+            description: taskDescription,
+            priority: taskPriority,
+            status: "In Progress"
+        }
+        if (newTask.name !== "") {
+            onSave(newTask);
+        } else {
+            alert("Please enter the Task Name");
+        }
+    });
+
+    useEffect(() => {
+        if (show) {
+            setTaskName("");
+            setTaskDescription("");
+            setTaskPriority("High");
+        }
+    }, [show]);
+
     return (
         <BootstrapModal show={show} onHide={onClose}>
             <BootstrapModal.Header closeButton>
                 <BootstrapModal.Title>Add Task</BootstrapModal.Title>
             </BootstrapModal.Header>
             <BootstrapModal.Body>
-
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>Task Name</InputGroup.Text>
+                    <Form.Control placeholder="Enter task name" value={taskName} onChange={(e) => {setTaskName(e.target.value)}}/>
+                </InputGroup>
+                <InputGroup>
+                    <InputGroup.Text>Task Description</InputGroup.Text>
+                    <Form.Control as="textarea" placeholder="Enter task description" value={taskDescription} onChange={(e) => {setTaskDescription(e.target.value)}}></Form.Control>
+                </InputGroup>
+                <InputGroup className="mt-3">
+                    <InputGroup.Text>Task Priority</InputGroup.Text>
+                    <Form.Select value={taskPriority} onChange={(e) => {setTaskPriority(e.target.value)}}>
+                        <option>High</option>
+                        <option>Medium</option>
+                        <option>Low</option>
+                    </Form.Select>
+                </InputGroup>
             </BootstrapModal.Body>
             <BootstrapModal.Footer>
                 <Button variant="secondary" onClick={onClose}>Close</Button>
-                <Button variant="primary">Save Task</Button>
+                <Button variant="primary" onClick={handleSave}>Save Task</Button>
             </BootstrapModal.Footer>
         </BootstrapModal>
     );
--- my-to-do-app/src/components/Todo.jsx
+++ my-to-do-app/src/components/Todo.jsx
@@ -4,29 +4,64 @@ import Modal from "./Modal";
 
 export default function Todo() {
     const [showModal, setShowModal] = useState(false);
+    const [tasks, setTasks] = useState([]);
+
+    const [search, setSearch] = useState("");
 
     const handleOpenModal = () => setShowModal(true);
     const handleCloseModal = () => setShowModal(false);
+
+    // Добавление новой задачи в таблицу
+    const handleSaveModal = (newTask) => {
+        setTasks([...tasks, newTask]);
+        handleCloseModal();
+    }
+
+    // Удаление задачи
+    const handleDeleteTask = (taskIndex) => {
+        const updatedTasks = tasks.filter(
+            (item, index) => taskIndex !== index
+        )
+        const clarify = confirm("Are you sure you want to delete the task?");
+
+        if (clarify) {
+            setTasks(updatedTasks);
+        }
+    }
+    
+    // Изменение задачи
+    const handleEditTask = () => {
+        handleOpenModal();
+    }
+
+    // Завершение задачи
+    const handleCompleteTask = (taskIndex) => {
+        const notCompletedTasks = tasks.filter(
+            (item, index) => taskIndex !== index
+        );
+        setTasks(notCompletedTasks);
+    }
     
     return (
-        <div className="bg-white rounded shadow-lg p-5 w-75 h-75">
+        <div className="bg-white rounded shadow-lg p-5 w-75 mh-75">
             <div className="d-flex justify-content-center align-items-center">
                 <img className="w-25" src={taskIcon} alt="Task Icon"></img>
                 <h1 className="justify-self-center">To-Do App</h1>
             </div>
             <div className="d-flex justify-content-between align-items-center m-3">
                 <button className="btn btn-primary btn-lg" onClick={handleOpenModal}>+ Add Task</button>
-                <input type="text" className="form-control w-25" placeholder="Search Task..."></input>
+                <input type="text" className="form-control w-25" placeholder="Search Task..." value={search} onChange={(e) => setSearch(e.target.value)}></input>
                 <select className="form-select h-25 w-25">
                     <option>All</option>
                     <option>In Progress</option>
                     <option>Complete</option>
                 </select>
-                <select className="form-select h-25 w-25">
+                <select className="form-select h-25 w-25" id="prioritySort">
                     <option>None</option>
                     <option>High to Low</option>
                     <option>Low to High</option>
                 </select>
+
             </div>
             <div className="mt-5">
                 <table className="table table-striped table-bordered table-hover">
@@ -34,27 +69,31 @@ export default function Todo() {
                         <tr>
                             <th scope="col">Task Name</th>
                             <th scope="col">Task Description</th>
-                            <th scope="col">Task Status</th>
                             <th scope="col">Task Priority</th>
+                            <th scope="col">Task Status</th>
                             <th scope="col">Actions</th>
                         </tr>
                     </thead>
                     <tbody className="table-group-divider">
-                        <tr>
-                            <th scope="row">Task 1</th>
-                            <td>This is a task to do</td>
-                            <td>In Progress</td>
-                            <td>High</td>
-                            <td className="d-flex justify-content-around">
-                                <button className="btn btn-success">Complete</button>
-                                <button className="btn btn-warning">Edit</button>
-                                <button className="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
+                        {tasks.filter((task) => (
+                            search.toLowerCase() === "" ? task : task.name.toLowerCase().includes(search) || task.description.toLowerCase().includes(search)
+                        )).map((task, index) => (
+                            <tr key={index}>
+                                <th scope="row">{task.name}</th>
+                                <td>{task.description}</td>
+                                <td>{task.priority}</td>
+                                <td>{task.status}</td>
+                                <td className="d-flex justify-content-around">
+                                    <button className="btn btn-success" onClick={() => handleCompleteTask(index)}>Complete</button>
+                                    <button className="btn btn-warning" onClick={handleEditTask}>Edit</button>
+                                    <button className="btn btn-danger" onClick={() => handleDeleteTask(index)}>Delete</button>
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
-            <Modal show={showModal} onClose={handleCloseModal}></Modal>
+            <Modal show={showModal} onClose={handleCloseModal} onSave={handleSaveModal}></Modal>
         </div>
     );
 }
